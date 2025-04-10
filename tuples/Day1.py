080f5192b56aef8f62b4b557633c63049b326a4f
ZahirSeid <zahirseid101@gmail.com>
2025-04-10 13:15:35 +0300

    Day 1 , we saw about index and count method in tuples

---
+++ tuples/Day1.py
@@ -0,0 +1,15 @@
+tuples_example = ('banana', 'apples', 'orange', 'banana')
+
+print(tuples_example.count('banana')) # this will count how many occurence does the sting have in the tuple
+
+print(tuples_example.index('orange')) # the index method will allow us to identify the index of the string object 
+
+print(tuples_example.index('banana')) # this will only print out for us the first occurence of the string in the tuple 
+
+# output
+
+"""
+2
+2
+0
+"""
